a26e841b718c3dd22d64e6f2ae470e071ed80e9f
Ben Teichman <ben.l.teichman@gmail.com>
2017-08-01 00:35:36 -0400

    test(package): switch to @storefront/testing

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -1,10 +1,8 @@
+import { bootstrap } from '@storefront/testing';
 import * as chai from 'chai';
-import * as mock from 'mock-require';
-import * as sinonChai from 'sinon-chai';
 
-chai.use(sinonChai);
-
-mock('../src/pager/index.html', {});
-mock('../src/pages/index.html', {});
-mock('../src/paging/index.html', {});
-mock('../src/terminal-pager/index.html', {});
+bootstrap(chai, __dirname, ['../src/pager/index.html',
+  '../src/pages/index.html',
+  '../src/paging/index.html',
+  '../src/terminal-pager/index.html',
+]);
